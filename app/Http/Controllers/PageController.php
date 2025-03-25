c1d97a39edd2ffdb1a239876c043879017aec48c
KianFratz <fratzkian@gmail.com>
2025-03-25 23:02:51 +0800

    Add some comment in page controller about function

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    // Display the about us page
     public function about()
     {
         return view('about');
